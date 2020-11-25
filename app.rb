8e8218b7d39662378786fe67d1f2f7c703a70195
joshsrai <joshsrai@gmail.com>
2020-11-25 13:15:04 -0500

    commit tree 329c977278d4187cfaec275129c1318bbebb5ed9

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, Worldfew!"
+      end 
+
+      get '/name' do
+        "My name is Waldo"
+      end 
+
+      get '/hometown' do
+        "My hometown is Thereville"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is 4:33"
+      end 
+
 end
